029d36bf8de8a9d7b4537210ad69dfab5db9c8d4
sara yasser <saraelgazaly2004@gmail.com>
2024-09-08 18:43:45 +0300

    Add project files.

---
+++ vs-2022-github-user-experience/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using vs_2022_github_user_experience.Models;
+
+namespace vs_2022_github_user_experience.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ vs-2022-github-user-experience/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vs_2022_github_user_experience.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
